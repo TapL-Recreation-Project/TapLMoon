01079fdeabf13fb73adadb0631d72f1fdae0fe99
Caio P <caiopq98@gmail.com>
2021-04-06 19:20:31 -0300

    Fix nether portals not working in the nether

--- src/main/java/dev/barfuzzle99/taplmoon/taplmoon/PortalListener.java
+++ src/main/java/dev/barfuzzle99/taplmoon/taplmoon/PortalListener.java
@@ -28,16 +28,30 @@ public class PortalListener implements Listener {
                 event.setTo(Bukkit.getWorld("moon_the_end").getSpawnLocation());
                 break;
             case NETHER_PORTAL:
-                World nether = Bukkit.getWorld("moon_nether");
-                if (nether == null) {
-                    TaplMoon.getInstance().getLogger().log(Level.SEVERE, "could not find moon_nether, did you rename or delete the world?");
-                    return;
+                boolean isEnteringNether = event.getPlayer().getWorld().getName().equals("moon");
+                if (isEnteringNether) {
+                    World nether = Bukkit.getWorld("moon_nether");
+                    if (nether == null) {
+                        TaplMoon.getInstance().getLogger().log(Level.SEVERE, "could not find 'moon_nether', did you rename or delete the world?");
+                        return;
+                    }
+                    Location netherLoc = event.getPlayer().getLocation().clone();
+                    netherLoc.setX(netherLoc.getX() / 8);
+                    netherLoc.setZ(netherLoc.getZ() / 8);
+                    netherLoc.setWorld(nether);
+                    event.setTo(netherLoc);
+                } else {
+                    World overworld = Bukkit.getWorld("moon_nether");
+                    if (overworld == null) {
+                        TaplMoon.getInstance().getLogger().log(Level.SEVERE, "could not find 'moon', did you rename or delete the world?");
+                        return;
+                    }
+                    Location overworldLoc = event.getPlayer().getLocation().clone();
+                    overworldLoc.setX(overworldLoc.getX() * 8);
+                    overworldLoc.setZ(overworldLoc.getZ() * 8);
+                    overworldLoc.setWorld(overworld);
+                    event.setTo(overworldLoc);
                 }
-                Location netherLoc = event.getPlayer().getLocation().clone();
-                netherLoc.setX(netherLoc.getX() / 8);
-                netherLoc.setZ(netherLoc.getZ() / 8);
-                netherLoc.setWorld(nether);
-                event.setTo(netherLoc);
                 break;
             default:
                 break;
